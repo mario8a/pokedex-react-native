1f82b0f4787fc6987cc0896bf8debb65b5ba2bb7
mario8a <mario8ato@hotmail.com>
2021-12-16 22:24:09 -0600

    setFavs

--- src/screens/Favorite.js
+++ src/screens/Favorite.js
@@ -1,10 +1,18 @@
-import React from 'react'
-import { View, Text, SafeAreaView } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { View, Text, SafeAreaView, Button } from 'react-native'
+import { getPokemonsFavoriteApi } from '../api/favorite'
 
 export default function Favorite() {
+
+  const checkFavorites = async () =>  {
+    const response = await getPokemonsFavoriteApi();
+    console.log(response);
+  }
+
   return (
     <SafeAreaView>
       <Text>Favorite</Text>
+      <Button title="Obtener favoritos" onPress={checkFavorites} />
     </SafeAreaView>
   )
 }
--- src/screens/Pokemon.js
+++ src/screens/Pokemon.js
@@ -2,18 +2,21 @@ import React, { useEffect, useState } from 'react'
 import { View, Text, ScrollView } from 'react-native'
 import Icon from "react-native-vector-icons/FontAwesome5";
 import { getPokemonDetailsApi } from '../api/pokemonApi';
+import Favorite from '../components/Pokemon/Favorite';
 import Header from '../components/Pokemon/Header';
 import Stats from '../components/Pokemon/Stats';
 import Type from '../components/Pokemon/Type';
+import useAuth from '../hooks/useAuth';
 
 export default function Pokemon(props) {
   const { navigation, route: {params} } = props;
 
   const [pokemon, setPokemon] = useState(null);
+  const { auth } = useAuth();
 
   useEffect(() => {
     navigation.setOptions({
-      headerRight: () => null,
+      headerRight: () => auth && <Favorite id={pokemon?.id}/>,
       headerLeft: () => (
         <Icon
           name="arrow-left"
@@ -24,7 +27,7 @@ export default function Pokemon(props) {
         />
       ),
     });
-  }, [navigation, params]);
+  }, [navigation, params, pokemon]);
 
   useEffect(() => {
     (async () => {
